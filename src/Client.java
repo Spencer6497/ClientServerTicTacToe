e5f57b0fbe2e03faaa80dabbe703c522c2e94430
Spencer Peace <peaces2@nku.edu>
2020-11-19 11:15:18 -0500

    Program is now complete in its entirety and works as intended, including with concurrency. Final commit with decompiled .class files.

--- src/Client.java
+++ src/Client.java
@@ -7,20 +7,131 @@
 
 import java.net.*;
 import java.io.*;
+import java.util.Scanner;
 
 public class Client {
-    private DataInputStream instream;
-    private DataOutputStream outstream;
-    private PrintWriter out;
-    private BufferedReader in;
+    private static Socket toServer;
+    private static char[][] board;
+    private static int row, col;
+    private static DataInputStream instream;
+    private static DataOutputStream outstream;
+    private static PrintWriter out;
+    private static BufferedReader in;
+
+    public Client() {}
 
     public static void main(String[] args) {
         try {
-            System.out.println("Starting new game of Tic-Tac-Toe...");
-            System.out.println("Attempting to connect to server on port 7788...");
-            Socket toServ = new Socket("localhost", 7788);
+            toServer = new Socket("localhost", 7788);
+            instream = new DataInputStream(toServer.getInputStream());
+            outstream = new DataOutputStream(toServer.getOutputStream());
+            out = new PrintWriter(outstream, true);
+            in = new BufferedReader(new InputStreamReader(instream));
+            board = new char[3][3];
         } catch (IOException e) {
-            // Placeholder for error handling
+            System.out.println("Problem initializing Client: " + e);
+            e.printStackTrace();
+        }
+
+        // Initialize board and row/col values
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = ' ';
+            }
+        }
+        row = col = -1;
+
+        // Begin game
+        ticTacToe(in, out);
+    }
+
+    public static void ticTacToe(BufferedReader in, PrintWriter out) {
+        Scanner scn = new Scanner(System.in);
+        // Boolean variable to yield control of game to server or client
+        boolean ctrl = false;
+
+        // Begin iteration over game
+        for (boolean bool = false; !bool; ctrl = !ctrl) {
+            // Give control to server first
+            if (!ctrl) {
+                String response = "";
+                try {
+                    response = in.readLine();
+                } catch (IOException e) {
+                    System.out.println("Unable to read server response: " + e);
+                    e.printStackTrace();
+                }
+                // Server has chosen to move first
+                if (!response.equals("NONE")) {
+                    String[] splitResponse = response.split("\\s+");
+                    // If server response is a win/tie/loss message
+                    if (splitResponse.length > 3) {
+                        row = Integer.parseInt(splitResponse[1]);
+                        col = Integer.parseInt(splitResponse[2]);
+                        // If tie or loss, still print server's X
+                        if (!splitResponse[3].equals("WIN") && row != -1) {
+                            board[row][col] = 'X';
+                        }
+
+                        // Break down server response, pick out game end keyword (win/loss/tie)
+                        String endCondition = splitResponse[3];
+                        if (endCondition.equals("TIE")) {
+                            System.out.println("\nThe game was a TIE!");
+                            break;
+                        } else if (endCondition.equals("WIN")) {
+                            System.out.println("\n\nCongratulations!!! You WON the game!");
+                            break;
+                        } else if (endCondition.equals("LOSS")) {
+                            System.out.println("\nSORRY! You LOST the game!");
+                            break;
+                        }
+
+                        bool = true;
+                    // Server's response was a move message; update the board accordingly
+                    } else {
+                        row = Integer.parseInt(splitResponse[1]);
+                        col = Integer.parseInt(splitResponse[2]);
+                        board[row][col] = 'X';
+                        System.out.println("\n\nSERVER'S MOVE: ");
+                        printBoard();
+                    }
+                // Player moves first
+                } else {
+                    System.out.println("\nYOU MOVE FIRST");
+                    printBoard();
+                }
+            // Pass control to client
+            } else {
+                while (true) {
+                    do {
+                        System.out.print("\nEnter Row: ");
+                        row = scn.nextInt();
+                        System.out.print("Enter Column: ");
+                        col = scn.nextInt();
+                    } while (row < 0);
+
+                    if (row <= 2 && col <= 2 && col >= 0 && board[row][col] == ' ') {
+                        board[row][col] = 'O';
+                        System.out.println("\n\nYOUR MOVE: ");
+                        printBoard();
+                        out.println("MOVE " + row + " " + col);
+                        break;
+                    }
+                }
+            }
+        }
+
+        System.out.println("\n\nHere is the final game board: ");
+        printBoard();
+    }
+
+    private static void printBoard() {
+        for (int i = 0; i < 3; i++) {
+            System.out.println(board[i][0] + " | " + board[i][1] + " | " + board[i][2]);
+            // Print horizontal lines
+            if (i != 2) {
+                System.out.println("----------");
+            }
         }
     }
 }
--- src/Server_thread.java
+++ src/Server_thread.java
@@ -26,7 +26,7 @@ public class Server_thread extends Thread {
             gen = new Random();
             instream = new DataInputStream(toClient.getInputStream());
             outstream = new DataOutputStream(toClient.getOutputStream());
-            out = new PrintWriter(outstream);
+            out = new PrintWriter(outstream, true);
             in = new BufferedReader(new InputStreamReader(instream));
             board = new char[3][3];
             // Nested loop over board to initialize values as blanks
@@ -55,11 +55,15 @@ public class Server_thread extends Thread {
         int rand = gen.nextInt();
         turn = rand % 2 == 0;
 
+        // if it's the player's turn first, alert the player of this
+        if (turn) {
+            out.println("NONE");
+        }
+
         // Continue playing until gameOver
         while (!gameOver) {
             // Player's Move
             if (turn) {
-                out.println("NONE");
                 try {
                     response = in.readLine();
                 } catch (IOException e) {
@@ -71,6 +75,7 @@ public class Server_thread extends Thread {
                 col = Integer.parseInt(data[2]);
                 // Place player's O into specified matrix spot
                 board[row][col] = 'O';
+                System.out.println("\n\nPLAYER'S MOVE: ");
                 printBoard();
                 counter++;
 
@@ -88,36 +93,33 @@ public class Server_thread extends Thread {
                 makeMove();
                 counter++;
                 board[row][col] = 'X';
+                System.out.println("\n\nSERVER'S MOVE: ");
                 printBoard();
 
                 // Check if server win or tie
                 if (checkWin() || counter == 9) {
                     gameOver = true;
                     if (checkWin()) {
-                        out.printf("MOVE %o %o LOSS", row, col);
+                        out.println("MOVE " + row + " " + col + " LOSS");
                     } else {
-                        out.printf("MOVE %o %o TIE", row, col);
+                        out.println("MOVE " + row + " " + col + " TIE");
                     }
                 // Move did not win game
                 } else {
-                    out.printf("MOVE %o %o", row, col);
+                    out.println("MOVE " + row + " " + col);
                 }
-                // Change turn from server to client
-                turn = !turn;
             }
+            // Change turn from server to client
+            turn = !turn;
         }
     }
 
     // Method to print game board
     private void printBoard() {
         for (int i = 0; i < 3; i++) {
-            out.println(board[i][0] + " | " + board[i][1] + " | " + board[i][2]);
-            // Test print
             System.out.println(board[i][0] + " | " + board[i][1] + " | " + board[i][2]);
             // Print horizontal lines
             if (i != 2) {
-                out.println("----------");
-                // Test print
                 System.out.println("----------");
             }
         }
@@ -125,21 +127,19 @@ public class Server_thread extends Thread {
 
     // Method to check if current board contains win
     private boolean checkWin() {
-        for (int i = 0; i < 3; i++) {
-            // Check for top left -> bottom right diagonal win
-            if (i == 0) {
-                if (board[i][0] == board[i + 1][1] && board[i + 1][1] == board[i + 2][2] && board[i][0] != ' ') {
+        // Check for top left -> bottom right diagonal win
+        if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != ' ') {
+            return true;
+        // Check for bottom left -> top right diagonal win
+        } else if (board[2][0] == board[1][1] && board[1][1] == board[0][2] && board[2][0] != ' ') {
+            return true;
+        } else {
+            for (int i = 0; i < 3; i++) {
+                // Check for row-win
+                if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != ' ') {
                     return true;
-                }
-            // Check for row-win
-            } else if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != ' ') {
-                return true;
-            // Check for col-win
-            } else if (board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[0][i] != ' ') {
-                return true;
-            // Check for bottom left -> top right diagonal win
-            } else if (i == 2) {
-                if (board[i][0] == board[i - 1][1] && board[i - 1][1] == board[i - 2][2] && board[i][0] != ' ') {
+                    // Check for col-win
+                } else if (board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[0][i] != ' ') {
                     return true;
                 }
             }
